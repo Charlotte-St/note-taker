21fb68d4357fab9b1396cd50699d1efaf6a3311e
Lotte-Et <charlotte.stowe@gmail.com>
2024-03-02 17:11:28 -0500

    Added route to get db.json

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
-const apiRoutes = require('./public/assets/js/routes/apiRoutes');
+//const apiRoutes = require('./public/assets/js/routes/apiRoutes');
 //const htmlRoutes = require('./public/assets/js/routes/htmlRoutes');
+const api = require('./public/assets/js/routes/index')
 
 const app = express();
 
 const PORT = 3001;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use('/api', api);
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
